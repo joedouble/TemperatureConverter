8f232566d0b0ba69ad9e1576316dd8e32eba6449
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 10:05:27 -0500

    Committing my original code from the first time I did this homework assignment.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double tempInFahrenheit = 100;
+        double tempInCelsius = (tempInFahrenheit - 32) / 1.8;
+
+        System.out.println("The temperature in Fahrenheit is " + tempInFahrenheit);
+        System.out.println("The temperature in Celsius is " + tempInCelsius);
+    }
+}
