959652a85c1c7c686c7d77d096bb27e5aca521eb
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 10:10:27 -0500

    Refactoring code to allo use user input of the temp in Fahrenheit.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        double tempInFahrenheit = 100;
-        double tempInCelsius = (tempInFahrenheit - 32) / 1.8;
 
-        System.out.println("The temperature in Fahrenheit is " + tempInFahrenheit);
-        System.out.println("The temperature in Celsius is " + tempInCelsius);
+        double tempInFahrenheit = 0;
+        double tempInCelsius = 0;
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What is the temperature in Fahrenheit that you'd like converted to Celsius?");
+        tempInFahrenheit = myScanner.nextDouble();
+
+        tempInCelsius = (tempInFahrenheit - 32) / 1.8;
+
+        System.out.println("The temperature in Fahrenheit is " + tempInFahrenheit + ".");
+        System.out.println("The temperature in Celsius is " + tempInCelsius + ".");
     }
 }
